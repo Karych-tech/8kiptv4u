ab7b008d3f736910b0ec0e049886577d13d96fe9
Karych Abdellah <Abdellah.karych@etu.uae.ac.ma>
2025-12-21 17:55:51 +0100

    optima

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,7 +1,24 @@
+// NAVBAR TOGGLE  
+const navbartoggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+if (navbartoggle && navbarMenu) {
+  navbartoggle.addEventListener('click', () => {
+    navbartoggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+  });
+
+  document.addEventListener('click', (e) => {
+      // Close menu if click is outside of the menu and the toggle button
+      if (!navbarMenu.contains(e.target) && !navbartoggle.contains(e.target)) {
+          navbarMenu.classList.remove('active');
+          navbartoggle.classList.remove('active');
+      }
+  });
+}
 
 
 // --- HERO SECTION SWIPER ---
-
 var heroSwiper = new Swiper(".hero-swiper", {
 
   
