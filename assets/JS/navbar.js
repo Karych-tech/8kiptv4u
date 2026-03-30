9d1fa0999cf6a0529fd2a9323360973b001c7a88
Karych Abdellah <Abdellah.karych@etu.uae.ac.ma>
2026-03-30 01:19:53 +0100

    navbar improvement and changing faq size

--- assets/JS/navbar.js
+++ assets/JS/navbar.js
@@ -8,6 +8,14 @@ if (navbartoggle && navbarMenu) {
     navbarMenu.classList.toggle('active');
   });
 
+  // Close menu when a link inside the menu is clicked
+  navbarMenu.addEventListener('click', (e) => {
+    if (e.target.closest('a')) {
+      navbarMenu.classList.remove('active');
+      navbartoggle.classList.remove('active');
+    }
+  });
+
   document.addEventListener('click', (e) => {
       // Close menu if click is outside of the menu and the toggle button
       if (!navbarMenu.contains(e.target) && !navbartoggle.contains(e.target)) {
